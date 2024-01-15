70eb29a29fdfaff559831eef63173d4960d9df51
当自己耀眼的星星 <95392539+DUuOOO@users.noreply.github.com>
2024-01-15 20:42:53 +0800

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module webCrawler {
+    requires org.jsoup;
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.edge_driver;
+	requires java.sql;
+	//requires org.seleniumhq.selenium.chrome_driver;
+}
